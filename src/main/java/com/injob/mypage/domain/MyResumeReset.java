4b7e664619323a746b40e7a53ad8b1974480ced3
wodud6967 <wodud6967@naver.com>
2024-05-22 19:50:49 +0900

    이력서수정중

---
+++ src/main/java/com/injob/mypage/domain/MyResumeReset.java
@@ -0,0 +1,17 @@
+package com.injob.mypage.domain;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyResumeReset {
+
+	// resumeVo
+	private Long   po_id;
+	
+}
